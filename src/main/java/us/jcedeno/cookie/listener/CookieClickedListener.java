727e5cd14b0d0a485aea3ae76d530223049781e2
Juan Cedeno <cto@noobsters.net>
2021-12-10 12:44:34 -0500

    even more cleanup!

--- src/main/java/us/jcedeno/cookie/listener/CookieClickedListener.java
+++ src/main/java/us/jcedeno/cookie/listener/CookieClickedListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
@@ -34,69 +35,66 @@ public class CookieClickedListener implements Listener {
      * interact with it.
      */
     @EventHandler
-    public void onInteractAtEntity(PlayerInteractAtEntityEvent e) {
+    public void onInteractAtEntity(final PlayerInteractAtEntityEvent e) {
+        if (!CookieManager.EDIT)
+            return;
 
-        if (CookieManager.EDIT) {
-            var entity = e.getRightClicked();
+        final var entity = e.getRightClicked();
 
-            if (entity != null && entity instanceof ItemFrame frame) {
-                ItemStack itemInFrame = frame.getItem();
-                if (itemInFrame != null && itemInFrame.getType() == Material.FILLED_MAP
-                        && itemInFrame.getItemMeta() instanceof MapMeta map) {
+        if (entity != null && entity instanceof ItemFrame frame) {
+            ItemStack itemInFrame = frame.getItem();
+            if (itemInFrame != null && itemInFrame.getType() == Material.FILLED_MAP
+                    && itemInFrame.getItemMeta() instanceof MapMeta map) {
 
-                    var entry = cookieManager.getFrameMap().get(entity.getLocation().getBlock());
-                    if (entry != null) {
+                var entry = cookieManager.getFrameMap().get(entity.getLocation().getBlock());
+                if (entry != null) {
 
-                        var interactionPoint = entity.getLocation().add(e.getClickedPosition());
+                    var interactionPoint = entity.getLocation().add(e.getClickedPosition());
 
-                        Bukkit.getPluginManager().callEvent(new PlayerClickedCookieEvent(interactionPoint,
-                                e.getPlayer(), frame, !Bukkit.isPrimaryThread()));
+                    Bukkit.getPluginManager().callEvent(new PlayerClickedCookieEvent(interactionPoint,
+                            e.getPlayer(), frame, !Bukkit.isPrimaryThread()));
 
-                    }
                 }
-
             }
+
         }
     }
 
     /**
-     * Event called when a player clicks on a block which contains a map on a item
+     * Event called when a player clicks on a block which contains a map on an item
      * frame above of it.
      */
     @EventHandler
-    public void onPlayerInteractAtItemFramesBlock(PlayerInteractEvent e) {
+    public void onPlayerInteractAtItemFramesBlock(final PlayerInteractEvent e) {
+        if (!CookieManager.EDIT || e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getBlockFace() != BlockFace.UP)
+            return;
 
-        if (CookieManager.EDIT) {
-            if (e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getBlockFace() != BlockFace.UP) {
-                return;
+        var block = e.getClickedBlock().getRelative(BlockFace.UP);
+
+        var map = cookieManager.getFrameMap().get(block);
+        if (map != null) {
+            var interaction = e.getInteractionPoint();
+            if (interaction != null) {
+                // Normalize the vector and reduce it by one vector unit for accuracy.
+                var vec = interaction.toVector().clone();
+                var normie = e.getPlayer().getEyeLocation().toVector().subtract(vec).normalize();
+                var l = interaction.toVector().clone().add(normie.multiply(0.05)).toLocation(block.getWorld());
+                // Call the Event and handle everything else there.
+                Bukkit.getPluginManager()
+                        .callEvent(new PlayerClickedCookieEvent(l, e.getPlayer(), map, !Bukkit.isPrimaryThread()));
             }
-            var block = e.getClickedBlock().getRelative(BlockFace.UP);
-            // send location to player
 
-            var map = cookieManager.getFrameMap().get(block);
-            if (map != null) {
-                var interaction = e.getInteractionPoint();
-                if (interaction != null) {
-
-                    var vec = interaction.toVector().clone();
-                    var normie = e.getPlayer().getEyeLocation().toVector().subtract(vec).normalize();
-                    var l = interaction.toVector().clone().add(normie.multiply(0.05)).toLocation(block.getWorld());
-
-                    Bukkit.getPluginManager()
-                            .callEvent(new PlayerClickedCookieEvent(l, e.getPlayer(), map, !Bukkit.isPrimaryThread()));
-                }
-
-            }
         }
+
     }
 
-    @EventHandler
-    public void onPlayerClickedCookie(PlayerClickedCookieEvent e) {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerPaintedCookie(PlayerClickedCookieEvent e) {
+        // TODO Ensure that players can only edit a cookie if it belongs to them.
         if (CookieManager.EDIT) {
             var player = e.getPlayer();
             var frame = e.getItemFrame();
             var position = e.getInteractionPoint();
-            // TODO Ensure it works with negatives.
 
             var bX = frame.getLocation().getBlockX();
             var bZ = frame.getLocation().getBlockZ();
@@ -112,46 +110,43 @@ public class CookieClickedListener implements Listener {
 
             switch (frame.getRotation()) {
 
-            case NONE:
-            case FLIPPED: {
-                x = (int) (relativeX * 128);
-                // Add 1 - z to adjust for rotation.
-                z = (int) ((relativeZ) * 128);
-                packet = cookieMap.paintPixel(Math.min(127, x), Math.min(127, z), (byte) 24);
-                break;
-            }
-            case CLOCKWISE_45:
-            case FLIPPED_45:
+                case NONE:
+                case FLIPPED: {
+                    x = (int) (relativeX * 128);
+                    // Add 1 - z to adjust for rotation.
+                    z = (int) ((relativeZ) * 128);
+                    packet = cookieMap.paintPixel(Math.min(127, x), Math.min(127, z), (byte) 24);
+                    break;
+                }
+                case CLOCKWISE_45:
+                case FLIPPED_45:
 
-                z = (int) Math.ceil((position.getX()) * 128);
-                x = (int) Math.ceil((position.getZ()) * 128);
-                packet = cookieMap.paintPixel(Math.min(x, 127), Math.min(128 - z, 127), (byte) 24);
+                    z = (int) Math.ceil((position.getX()) * 128);
+                    x = (int) Math.ceil((position.getZ()) * 128);
+                    packet = cookieMap.paintPixel(Math.min(x, 127), Math.min(128 - z, 127), (byte) 24);
 
-                break;
-            case COUNTER_CLOCKWISE:
-            case CLOCKWISE:
+                    break;
+                case COUNTER_CLOCKWISE:
+                case CLOCKWISE:
 
-                x = (int) Math.ceil((position.getX()) * 128);
-                z = (int) Math.ceil((position.getZ()) * 128);
-                packet = cookieMap.paintPixel(Math.min(128 - x, 127), Math.min(128 - z, 127), (byte) 24);
+                    x = (int) Math.ceil((position.getX()) * 128);
+                    z = (int) Math.ceil((position.getZ()) * 128);
+                    packet = cookieMap.paintPixel(Math.min(128 - x, 127), Math.min(128 - z, 127), (byte) 24);
 
-                break;
-            case COUNTER_CLOCKWISE_45:
-            case CLOCKWISE_135:
+                    break;
+                case COUNTER_CLOCKWISE_45:
+                case CLOCKWISE_135:
 
-                z = (int) Math.ceil((position.getX()) * 128);
-                x = (int) Math.ceil((position.getZ()) * 128);
-                packet = cookieMap.paintPixel(Math.min(128 - x, 127), Math.min(127, z), (byte) 24);
+                    z = (int) Math.ceil((position.getX()) * 128);
+                    x = (int) Math.ceil((position.getZ()) * 128);
+                    packet = cookieMap.paintPixel(Math.min(128 - x, 127), Math.min(127, z), (byte) 24);
 
-                break;
+                    break;
             }
             if (packet != null) {
                 packet.broadcastPacket();
             }
         }
-
-        // player.sendMessage("You've clicked at interaction " + frame.toString());
-
     }
 
 }
